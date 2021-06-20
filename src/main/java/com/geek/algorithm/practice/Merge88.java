323bf0f45d783ef6ce1d4415b625cf0b57b969cf
nick-zhong123 <zhong_wei_cup@163.com>
2021-06-20 17:47:12 +0800

    88. 合并两个有序数组

---
+++ src/main/java/com/geek/algorithm/practice/Merge88.java
@@ -0,0 +1,74 @@
+package com.geek.algorithm.practice;
+
+/**
+ * @author nick
+ * @date 2021/6/20 2:08 PM
+ * @package com.geek.algorithm.practice
+ * @description
+ *  leetcode第88题
+ *      合并两数组
+ *      https://leetcode-cn.com/problems/merge-sorted-array/
+ */
+public class Merge88 {
+
+    /**
+     *
+     * 从前往后合并
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     *
+     */
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
+        int[] sorted = new int[m + n];
+        int cur;
+        int p1 = 0;
+        int p2 = 0;
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                cur = nums1[p1++];
+            } else {
+                cur = nums2[p2++];
+            }
+            sorted[p1 + p2 - 1] = cur;
+        }
+        for (int i = 0; i < m + n; i++) {
+            nums1[i] = sorted[i];
+        }
+    }
+
+    /**
+     *
+     * 从后往前合并
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int tail = m + n - 1;
+        int cur;
+        while(p1 >= 0 || p2 >= 0) {
+            if (p1 == -1) {
+                cur = nums2[p2--];
+            } else if (p2 == -1) {
+                cur = nums1[p1--];
+            } else if (nums1[p1] > nums2[p2]) {
+                cur = nums1[p1--];
+            } else {
+                cur = nums2[p2--];
+            }
+            nums1[tail - 1] = cur;
+        }
+    }
+
+}
--- src/main/java/com/geek/algorithm/practice/RmDuplicates.java
+++ src/main/java/com/geek/algorithm/practice/RmDuplicates.java
@@ -6,8 +6,30 @@ package com.geek.algorithm.practice;
  * @package com.geek.algorithm
  * @description
  *  第一周预习课
+ *   https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
  */
 public class RmDuplicates {
 
+    /**
+     *
+     *
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast - 1] != nums[fast]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
 
 }
