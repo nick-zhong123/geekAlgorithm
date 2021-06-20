7bb7bc6248173b31db6a89c0018f12918cecf4ef
nick-zhong123 <zhong_wei_cup@163.com>
2021-06-20 11:58:54 +0800

    init

---
+++ src/main/java/com/geek/algorithm/Entrance.java
@@ -0,0 +1,10 @@
+package com.geek.algorithm;
+
+/**
+ * @author nick
+ * @date 2021/6/20 11:54 AM
+ * @package com.geek.algorithm
+ * @description
+ */
+public class Entrance {
+}
---
+++ src/main/java/com/geek/algorithm/practice/RmDuplicates.java
@@ -0,0 +1,13 @@
+package com.geek.algorithm.practice;
+
+/**
+ * @author nick
+ * @date 2021/6/20 11:53 AM
+ * @package com.geek.algorithm
+ * @description
+ *  第一周预习课
+ */
+public class RmDuplicates {
+
+
+}
