2713a3d41fab0a49c147898e9815df68acd01e1a
nick-zhong123 <zhong_wei_cup@163.com>
2021-06-27 16:40:28 +0800

    第二周总结大纲

--- src/main/java/com/geek/algorithm/homework/week2/LRU146.java
+++ src/main/java/com/geek/algorithm/homework/week2/LRU146.java
@@ -62,7 +62,8 @@ public class LRU146 {
         if (this.map.containsKey(key)) {
             Node node = this.map.get(key);
             removeFromList(node);
-            insertIntoListHead(node.key, node.value);
+            // put可能key相同，但是value值不一样
+            insertIntoListHead(node.key, value);
         } else {
             insertIntoListHead(key, value);
         }
@@ -115,17 +116,23 @@ public class LRU146 {
     }
 
     public static void main(String[] args) {
-        LRU146 lru = new LRU146(3);
-        // 初始化三个值
-        lru.put(1, 11); lru.printLRU();
-        lru.put(2, 22); lru.printLRU();
-        lru.put(3, 33); lru.printLRU();
-        // 超过容量，将key 1删除
-        lru.put(4, 44); lru.printLRU();
-        // 再次put key 3, 3变成第一个节点
-        lru.put(3, 3); lru.printLRU();
-        // get key 2, 2变成第一个节点
+//        LRU146 lru = new LRU146(3);
+//        // 初始化三个值
+//        lru.put(1, 11); lru.printLRU();
+//        lru.put(2, 22); lru.printLRU();
+//        lru.put(3, 33); lru.printLRU();
+//        // 超过容量，将key 1删除
+//        lru.put(4, 44); lru.printLRU();
+//        // 再次put key 3, 3变成第一个节点
+//        lru.put(3, 3); lru.printLRU();
+//        // get key 2, 2变成第一个节点
+//        lru.get(2); lru.printLRU();
+        LRU146 lru = new LRU146(2);
+        lru.put(2, 1); lru.printLRU();
+        lru.put(2, 2); lru.printLRU();
         lru.get(2); lru.printLRU();
-
+        lru.put(1, 1); lru.printLRU();
+        lru.put(4, 1); lru.printLRU();
+        lru.get(2); lru.printLRU();
     }
 }
