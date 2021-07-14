8383179da44a229b5562603c6f2a480abdcb9870
nick-zhong123 <zhong_wei_cup@163.com>
2021-07-14 20:40:32 +0800

    数据流的中位数295

---
+++ src/main/java/com/geek/algorithm/homework/week4/FindMedianFromDataStream295.java
@@ -0,0 +1,55 @@
+package com.geek.algorithm.homework.week4;
+
+import java.util.PriorityQueue;
+
+/**
+ *
+ *
+ * @author nick
+ * @date 2021/7/14 8:35 PM
+ * @package com.geek.algorithm.homework.week4
+ * @description
+ *  295. 数据流的中位数
+ *  https://leetcode-cn.com/problems/find-median-from-data-stream/
+ *  参考了标准答案，使用了现成的优先队列
+ *
+ */
+public class FindMedianFromDataStream295 {
+    /**
+     * 当前大顶堆和小顶堆的元素个数之和
+     */
+    private int count;
+    private PriorityQueue<Integer> maxheap;
+    private PriorityQueue<Integer> minheap;
+
+    /**
+     * initialize your data structure here.
+     */
+    public FindMedianFromDataStream295() {
+        count = 0;
+        maxheap = new PriorityQueue<>((x, y) -> y - x);
+        minheap = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        count += 1;
+        maxheap.offer(num);
+        minheap.add(maxheap.poll());
+        // 如果两个堆合起来的元素个数是奇数，小顶堆要拿出堆顶元素给大顶堆
+        if ((count & 1) != 0) {
+            maxheap.add(minheap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if ((count & 1) == 0) {
+            // 如果两个堆合起来的元素个数是偶数，数据流的中位数就是各自堆顶元素的平均值
+            return (double) (maxheap.peek() + minheap.peek()) / 2;
+        } else {
+            // 如果两个堆合起来的元素个数是奇数，数据流的中位数大顶堆的堆顶元素
+            return (double) maxheap.peek();
+        }
+    }
+}
+
+
