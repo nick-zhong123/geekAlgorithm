c6222434f90245e9d70f5af163bac4e94807f033
nick-zhong123 <zhong_wei_cup@163.com>
2021-08-04 21:22:25 +0800

    200. 岛屿数量

--- src/main/java/com/geek/algorithm/homework/week07/NumberOfIslands200.java
+++ src/main/java/com/geek/algorithm/homework/week07/NumberOfIslands200.java
@@ -7,39 +7,81 @@ package com.geek.algorithm.homework.week07;
  */
 public class NumberOfIslands200 {
 
-    void dfs(char[][] grid, int r, int c) {
-        int nr = grid.length;
-        int nc = grid[0].length;
-
-        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
-            return;
-        }
-
-        grid[r][c] = '0';
-        dfs(grid, r - 1, c);
-        dfs(grid, r + 1, c);
-        dfs(grid, r, c - 1);
-        dfs(grid, r, c + 1);
-    }
+    private int rows;
+    private int cols;
 
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
+        rows = grid.length;
+        if (rows == 0) {
             return 0;
         }
+        cols = grid[0].length;
 
-        int nr = grid.length;
-        int nc = grid[0].length;
-        int num_islands = 0;
-        for (int r = 0; r < nr; ++r) {
-            for (int c = 0; c < nc; ++c) {
-                if (grid[r][c] == '1') {
-                    ++num_islands;
-                    dfs(grid, r, c);
+        // 空地的数量
+        int spaces = 0;
+        UnionFind unionFind = new UnionFind(rows * cols);
+        int[][] directions = {{1, 0}, {0, 1}};
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '0') {
+                    spaces++;
+                } else {
+                    // 此时 grid[i][j] == '1'
+                    for (int[] direction : directions) {
+                        int newX = i + direction[0];
+                        int newY = j + direction[1];
+                        // 先判断坐标合法，再检查右边一格和下边一格是否是陆地
+                        if (newX < rows && newY < cols && grid[newX][newY] == '1') {
+                            unionFind.union(getIndex(i, j), getIndex(newX, newY));
+                        }
+                    }
                 }
             }
         }
+        return unionFind.getCount() - spaces;
+    }
 
-        return num_islands;
+    private int getIndex(int i, int j) {
+        return i * cols + j;
+    }
+
+    private class UnionFind {
+        /**
+         * 连通分量的个数
+         */
+        private int count;
+        private int[] parent;
+
+        public int getCount() {
+            return count;
+        }
+
+        public UnionFind(int n) {
+            this.count = n;
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        private int find(int x) {
+            while (x != parent[x]) {
+                parent[x] = parent[parent[x]];
+                x = parent[x];
+            }
+            return x;
+        }
+
+        public void union(int x, int y) {
+            int xRoot = find(x);
+            int yRoot = find(y);
+            if (xRoot == yRoot) {
+                return;
+            }
+
+            parent[xRoot] = yRoot;
+            count--;
+        }
     }
 
 }
