fd21d031814be06300fedd57ea9d3ab667e72cba
nick-zhong123 <zhong_wei_cup@163.com>
2021-08-18 23:08:45 +0800

    239. 滑动窗口最大值

---
+++ src/main/java/com/geek/algorithm/homework/week9/SlidingWindowMaximum239.java
@@ -0,0 +1,36 @@
+package com.geek.algorithm.homework.week9;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ *
+ * 239. 滑动窗口最大值
+ *      https://leetcode-cn.com/problems/sliding-window-maximum/
+ *
+ */
+public class SlidingWindowMaximum239 {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            public int compare(int[] pair1, int[] pair2) {
+                return pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1];
+            }
+        });
+        for (int i = 0; i < k; ++i) {
+            pq.offer(new int[]{nums[i], i});
+        }
+        int[] ans = new int[n - k + 1];
+        ans[0] = pq.peek()[0];
+        for (int i = k; i < n; ++i) {
+            pq.offer(new int[]{nums[i], i});
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+            ans[i - k + 1] = pq.peek()[0];
+        }
+        return ans;
+    }
+
+}
