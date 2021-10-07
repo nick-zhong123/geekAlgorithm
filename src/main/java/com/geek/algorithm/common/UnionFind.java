4814ea7a6020bd25b8ba0a7dd46a9f5d5a8575bd
nick-zhong123 <zhong_wei_cup@163.com>
2021-10-07 21:08:24 +0800

    并查集

--- src/main/java/com/geek/algorithm/common/UnionFind.java
+++ src/main/java/com/geek/algorithm/common/UnionFind.java
@@ -1,2 +1,49 @@
-package com.geek.algorithm.common;public class UnionFind {
+package com.geek.algorithm.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * 并查集基本操作
+ *
+ */
+public class UnionFind {
+
+    private Map<Integer, Integer> father;
+
+    public UnionFind() {
+        father = new HashMap<>();
+    }
+
+    public void add(int x) {
+        if (!father.containsKey(x)) {
+            father.put(x, null);
+        }
+    }
+
+    public void merge(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            father.put(rootX, rootY);
+        }
+    }
+
+    public int find(int x) {
+        int root = x;
+        while(father.get(root) != null) {
+            root = father.get(root);
+        }
+        while(x != root) {
+            int origin_father = father.get(x);
+            father.put(x, root);
+            x = origin_father;
+        }
+        return root;
+    }
+
+    public boolean isConnected(int x, int y) {
+        return find(x) == find(y);
+    }
 }
